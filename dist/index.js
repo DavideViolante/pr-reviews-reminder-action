19ee5b35044b438e6eb4a91cd398db44792eea01
DavideViolante <fungtq3@gmail.com>
2021-01-16 14:38:05 +0100

    Forgot to run build

--- dist/index.js
+++ dist/index.js
@@ -469,7 +469,7 @@ function stringToObject(str) {
   if (!str) {
     return map;
   }
-  const users = (str || '').split(',');
+  const users = str.split(',');
   users.forEach(user => {
     const [github, provider] = user.split(':');
     map[github] = provider
